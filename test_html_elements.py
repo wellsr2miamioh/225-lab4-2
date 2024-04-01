979bf7f001c4d1fedd784e5b9b463cc2be668297
Anthony W Rose <83881859+miamioh-roseaw@users.noreply.github.com>
2024-04-01 12:25:37 -0400

    Update test_html_elements.py

--- test_html_elements.py
+++ test_html_elements.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options
 import unittest
 
-class TestH5Tag(unittest.TestCase):
+class TestContacts(unittest.TestCase):
     def setUp(self):
         # Setup Firefox options
         firefox_options = Options()
@@ -13,7 +13,7 @@ class TestH5Tag(unittest.TestCase):
         firefox_options.add_argument("--disable-dev-shm-usage")
         self.driver = webdriver.Firefox(options=firefox_options)
 
-    def test_h5_tag_content(self):
+    def test_contacts(self):
         driver = self.driver
         driver.get("http://10.48.10.116")  # Replace with your target website
         
